4e5a8c7e6df94744c8fe470223b884643d8460c2
lucasualegre <l.alegre@galileo.edu.ar>
2024-08-15 21:11:24 -0300

    Update GodLileo.sql
    Tabla usuarios
    
    -cambio de la columna "rol" el campo  enum("cliente","administrador").
    
    - cambio de la columna "contrasenia" a varchar 100
    
    ---------------------------------------------------------------
    Tabla ventas
    
    -se unifico a solo order_id como varchar 20.
    
    ---------------------------------------------------------------
    Tabla pendientes
    
    -se agregaron las claves foráneas a las columnas "usuario_id","order_id","producto_id".
    
    -se modifico la columna "estado" por enum ("carrito","pendiente","entregado").

--- GodLileo.sql
+++ GodLileo.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tiempo de generación: 14-08-2024 a las 15:01:48
+-- Tiempo de generación: 16-08-2024 a las 00:00:12
 -- Versión del servidor: 5.6.20-log
--- Versión de PHP: 5.4.31
+-- Versión de PHP: 5.5.15
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Base de datos: `prueba`
+-- Base de datos: `asdasd`
 --
 
 -- --------------------------------------------------------
@@ -28,11 +28,11 @@ SET time_zone = "+00:00";
 
 CREATE TABLE IF NOT EXISTS `pendientes` (
 `pendiente_id` int(11) unsigned NOT NULL,
-  `usuario` varchar(30) NOT NULL,
-  `estado` varchar(10) NOT NULL,
+  `estado` enum('carrito','pendiente','entregado') NOT NULL,
   `producto_id` int(11) NOT NULL,
-  `order_id` varchar(20) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `order_id` varchar(20) NOT NULL,
+  `usuario_id` int(11) NOT NULL
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 -- --------------------------------------------------------
 
@@ -44,7 +44,15 @@ CREATE TABLE IF NOT EXISTS `productos` (
 `producto_id` int(11) unsigned NOT NULL,
   `descripcion` varchar(150) NOT NULL,
   `precio` decimal(10,2) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Volcado de datos para la tabla `productos`
+--
+
+INSERT INTO `productos` (`producto_id`, `descripcion`, `precio`) VALUES
+(1, 'zapatos', '10.00'),
+(2, 'celular', '10.00');
 
 -- --------------------------------------------------------
 
@@ -55,9 +63,17 @@ CREATE TABLE IF NOT EXISTS `productos` (
 CREATE TABLE IF NOT EXISTS `usuarios` (
 `usuario_id` int(10) unsigned NOT NULL,
   `nombre` varchar(30) NOT NULL,
-  `contrasenia` varchar(30) NOT NULL,
-  `rol` varchar(8) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `contrasenia` varchar(100) NOT NULL,
+  `rol` enum('administrador','cliente') NOT NULL
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`usuario_id`, `nombre`, `contrasenia`, `rol`) VALUES
+(1, 'sanz', '123', 'cliente'),
+(2, 'yo', '123', 'cliente');
 
 -- --------------------------------------------------------
 
@@ -77,7 +93,7 @@ CREATE TABLE IF NOT EXISTS `ventas` (
 -- Indices de la tabla `pendientes`
 --
 ALTER TABLE `pendientes`
- ADD PRIMARY KEY (`pendiente_id`), ADD KEY `producto_id` (`producto_id`), ADD KEY `orden_id` (`order_id`);
+ ADD PRIMARY KEY (`pendiente_id`), ADD KEY `producto_id` (`producto_id`), ADD KEY `orden_id` (`order_id`), ADD KEY `fk_usuario` (`usuario_id`), ADD KEY `order_id` (`order_id`), ADD KEY `estado` (`estado`), ADD KEY `pendiente_id` (`pendiente_id`);
 
 --
 -- Indices de la tabla `productos`
@@ -105,17 +121,17 @@ ALTER TABLE `ventas`
 -- AUTO_INCREMENT de la tabla `pendientes`
 --
 ALTER TABLE `pendientes`
-MODIFY `pendiente_id` int(11) unsigned NOT NULL AUTO_INCREMENT;
+MODIFY `pendiente_id` int(11) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT de la tabla `productos`
 --
 ALTER TABLE `productos`
-MODIFY `producto_id` int(11) unsigned NOT NULL AUTO_INCREMENT;
+MODIFY `producto_id` int(11) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-MODIFY `usuario_id` int(10) unsigned NOT NULL AUTO_INCREMENT;
+MODIFY `usuario_id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
